9363765728b9b5546c3736e3efd5b01647adcb3b
Jose Alcocer <jolual2747@gmail.com>
2023-11-05 22:25:04 -0500

    new CI/CD actions

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,7 @@ WORKDIR /app
 COPY . /app
 
 RUN pip install -r requirements.txt
+
+EXPOSE 8080
+
+CMD ["uvicorn", "app.app:app", "--host", "0.0.0.0", "--port", "8080"]
--- app/config/database.py
+++ app/config/database.py
@@ -13,6 +13,7 @@ db_password = urllib.parse.quote_plus(str(os.environ.get('DB_PASSWORD', 'secret'
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('SSL_MODE','prefer')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 engine = create_engine(DATABASE_URL, echo = True)
+print(DATABASE_URL)
 
 SessionLocal = sessionmaker(autoflush=False, autocommit = False, bind = engine)
 Base = declarative_base()
